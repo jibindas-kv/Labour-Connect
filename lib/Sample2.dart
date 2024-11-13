87c68b8a60ebb0359f06a0dd07d7b43bccb47e48
jibindas-kv <jibindas.work@gmail.com>
2024-11-13 08:03:15 -0800

    Create Sample2.dart

---
+++ lib/Sample2.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Second extends StatefulWidget {
+  const Second({super.key});
+
+  @override
+  State<Second> createState() => _HelloState();
+}
+
+class _HelloState extends State<Hello> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
