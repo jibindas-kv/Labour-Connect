d076e433e46c3b8b4722e267c48d5ed49d87d8d5
jibindaskv <jibindas.work@gmail.com>
2024-11-18 10:12:35 -0800

    First Commit

---
+++ lib/Customer/Customer_Login.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Customer_Login extends StatefulWidget {
+  const Customer_Login({super.key});
+
+  @override
+  State<Customer_Login> createState() => _Customer_LoginState();
+}
+
+class _Customer_LoginState extends State<Customer_Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Data"),
+    );
+  }
+}
